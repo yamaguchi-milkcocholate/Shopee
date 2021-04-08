6eed652521a0d7f93c93e8226c977ede0f22a8ac
yamaguchi-milkcocholate <zuuuubo.tetsu@outlook.jp>
2021-04-08 19:09:56 +0900

    [update] bert encode

--- src/text.py
+++ src/text.py
@@ -1,5 +1,6 @@
 import nltk
 import re
+import numpy as np
 
 
 def preprocess_text(text, flg_stemm=False, flg_lemm=True):
@@ -29,3 +30,28 @@ def preprocess_text(text, flg_stemm=False, flg_lemm=True):
     ## back to string from list
     text = " ".join(lst_text)
     return text
+
+
+# Return tokens, masks and segments from a text array or series
+def bert_encode(texts, tokenizer, max_len=512):
+    all_tokens = []
+    all_masks = []
+    all_segments = []
+    
+    for text in texts:
+        text = tokenizer.tokenize(text)
+
+        text = text[:max_len-2]
+        input_sequence = ["[CLS]"] + text + ["[SEP]"]
+        pad_len = max_len - len(input_sequence)
+        
+        tokens = tokenizer.convert_tokens_to_ids(input_sequence)
+        tokens += [0] * pad_len
+        pad_masks = [1] * len(input_sequence) + [0] * pad_len
+        segment_ids = [0] * max_len
+        
+        all_tokens.append(tokens)
+        all_masks.append(pad_masks)
+        all_segments.append(segment_ids)
+    
+    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
